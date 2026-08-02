7e28fb83baf8b028ef6f92f75d6643429ff38bae
Banti Kumar <raazaditya2001@gmail.com>
2026-08-02 21:36:32 +0530

    fix confif file og backend api

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const env = loadEnv(process.cwd(), "");
+
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [react(),
      tailwindcss(),
   ],
   server: {
     proxy :{
       "/api": {
-        target:import.meta.env.BACKEND_URL
+        target : env.VITE_API_URL,
       }
     }
   },
